b79ad0037597819bccf188d843d00afb23ee60df
AoneKom <aonestasy@gmail.com>
2024-11-26 09:01:51 +0100

    Delete sql.sql

--- sql.sql
+++
@@ -1,9 +0,0 @@
-CREATE DATABASE MariaDB;
-
-USE MariaDB;
-
-CREATE TABLE todos (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    task VARCHAR(255) NOT NULL,
-    status ENUM('nicht erledigt', 'erledigt') DEFAULT 'nicht erledigt'
-);
